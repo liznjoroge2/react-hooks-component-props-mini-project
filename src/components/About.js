a421de7a21662b237ca5a9771129f89689197bb4
enjoroge <njrogeelizabeth53@gmail.com>
2024-06-05 13:29:37 +0300

    commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+function About({ image = "https://via.placeholder.com/215", about }) {
+  return (
+    <aside>
+      <img src={image} alt="blog logo" 
+      placeholder="https://via.placeholder.com/215"
+      />
+      <p>{about}</p>
+    </aside>
+  );
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="My Blog" />
+      <About image="my-blog-logo.jpg" about="Welcome to my blog!" />
+      <ArticleList
+        posts={[
+          { title: "Post 1", date: "February 2, 2022", preview: "This is the preview of post 1" },
+          { title: "Post 2", date: "March 3, 2022", preview: "This is the preview of post 2" },
+          { title: "Post 3", preview: "This is the preview of post 3" },
+        ]}
+      />
     </div>
   );
 }
